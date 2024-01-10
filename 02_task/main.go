1ad3f12e66b73fcb4b0a5ccb929fc37b1eae8590
zuekveer <art.valid@mail.ru>
2024-01-10 17:11:19 +0300

    imported new packages in /02_task

--- 02_task/main.go
+++ 02_task/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"github.com/zuekveer/abcdWay/02_task/wordz"
+	newcolor "github.com/zuekveer/abcdWay/02_task/color"
 	"fmt"
-
 	"github.com/fatih/color"
 )
 
 func main() {
+	newcolor.Greet()
 	fmt.Println("Hello world")
 	color.Red("Hello world again")
 
--- 02_task/wordz/wordz.go
+++ 02_task/wordz/wordz.go
@@ -1,20 +1,24 @@
 package wordz
-
+//wordz make sense
 import ("crypto/rand"
 		"math/big"
+		"fmt"
 )
 
-var Hello = "This is package wordz" // says Hello
-var Prefix = "random word: " //says PRefix
-var Words = []string{"One", "Two", "Three", "Four", "Five"} //saysWords
+var Hello = "This is package wordz" //Hello initialize a sting
+var Prefix = "random word: " //Prefix makes it beatiful 
+var Words = []string{"One", "Two", "Three", "Four", "Five"} //Words says
 
-func Random() string { //makes Func
+func Random() string {  
 	max := len(Words)
 	r, _ := rand.Int(rand.Reader, big.NewInt(int64(max)))
 	return get(r.Int64()) 
-	}
+	} //Random aas
 
 	func get(index int64) string {
 		return Prefix + Words[index]
 		}
+	func init() {
+		fmt.Println("Function init in package wordz")
+	}
 	
